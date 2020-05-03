e6bdfd26af0646686d0fa9b213b1c0bf34e8d216
MikesoWeb <klionpsyx@gmail.com>
2020-05-03 20:35:12 +0300

    'add_move_func_and_html_files_css'

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from werkzeug.utils import html
+import os, shutil
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index()-> html:
-    name = 'Mike'
-    return render_template('index.html', n=name)
+    return render_template('index.html')
+
+@app.route('/entry', methods=['POST', 'GET'])
+def move_files():
+    path = request.form['in']
+    path2 = request.form['out']
+    global i, full_path
+    count = 0
+    extension_files = ['jpg', 'png', 'ico', 'svg', 'webp', 'gif']
+    for i in sorted(os.listdir(path)):
+
+        if (i.split('.')[-1]).lower() in extension_files:
+            shutil.move(path + i, path2 + i)
+            full_path = f'{path2}/{i}'
+
+            count += 1
+
+    return render_template('move_files.html',count=count, path=path, path2=path2)
 
 
+
+@app.route('/move_files')
+def move_files_page():
+    return render_template('move_files.html')
 
 
 if __name__=='__main__':
